90ca6332f00ea8dc76edc8386d4bd19d86660562
Adam DENG <dengnandong@yunzsp.com>
2017-11-29 22:37:14 +0800

    init project

---
+++ src/main/java/com/msyuns/irms/IrmsApplication.java
@@ -0,0 +1,12 @@
+package com.msyuns.irms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrmsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/msyuns/irms/SpringBootAdminLteTemplateWithSpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msyuns.irms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminLteTemplateWithSpringSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
